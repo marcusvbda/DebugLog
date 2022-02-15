87e2edec98c5d9cdae63a69767490ed2a0e28180
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-02-15 13:06:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const isDevelopment = (['development', 'test', 'testing'].includes(process?.env?.NODE_ENV || '')) || false;
+
+const debug = {
+  execute(type, ...content) {
+    if (isDevelopment) {
+      console[type](...content);
+    }
+  },
+  getActions() {
+    return ['error', 'log', 'table', 'assert', 'count', 'time', 'timeEnd', 'clear', 'dir'];
+  }
+};
+
+debug.getActions().forEach((type) => {
+  debug[type] = (...content) => {
+    debug.execute(type, ...content);
+  };
+});
+
+module.exports = debug;
